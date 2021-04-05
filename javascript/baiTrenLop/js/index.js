b62093c59ef1d2f1b13b2446fb37515538f2b93c
unknown <ngoclong3066@gmail.com>
2021-04-05 18:15:56 +0700

    l

---
+++ javascript/baiTrenLop/js/index.js
@@ -0,0 +1,11 @@
+function testExternal() {
+    document.getElementById("test_external").innerHTML = "hello ";
+}
+
+function change_content_2() {
+    document.getElementById("change_content").innerHTML = "Day la noi dung thu hai";
+}
+
+function changeImage() {
+    document.getElementById("change_image").src = "../img/ibrahimovic-se-quay-ve-man-utd1584935068.jpg";
+}
